a66fd589eae5ccbee69ac6791c5942498910ab4c
Matthias Bauer <moeffju@moeffju.net>
2013-03-13 18:23:23 +0100

    German version!

---
+++ cloud-zu-hintern/content_script.js
@@ -0,0 +1,55 @@
+walk(document.body);
+
+function walk(node) 
+{
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+  
+  var child, next;
+
+  switch ( node.nodeType )  
+  {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child ) 
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
+
+function handleText(textNode) 
+{
+  var v = textNode.nodeValue;
+
+  v = v.replace(/(?:(\u00fcber|gegen|in|f\u00fcr) )?(d)(er|ie) (C)loud\b/gi, function(match, case1, cap1, case2, cap2, offset, string) {
+    var r = '';
+    console.log([match, case1, cap1, case2, cap2]);
+    if (case1 == undefined) {
+      r += (cap1 == 'D' ? 'M' : 'm');
+      r += (case2 == 'er' ? 'einem ' : 'ein ');
+    }
+    else {
+      var is_upper = (case1.charAt(0).toUpperCase() == case1.charAt(0));
+      r += (is_upper ? case1.charAt(0).toUpperCase() + case1.slice(1) : case1) + ' ';
+      r += (cap1 == 'D' ? 'M' : 'm');
+      r += (case2 == 'er' ? 'einem ' : 'einen ');
+    }
+    r += (cap2 == 'C' ? 'H' : 'h');
+    r += 'intern';
+    return r;
+  });
+  
+  textNode.nodeValue = v;
+}
+
+
